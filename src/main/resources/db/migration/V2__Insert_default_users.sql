e4039928f4c33a0c6db3705a0493e832fd3f154a
Alexandre Barbosa <xandesbarbosa@gmail.com>
2025-10-28 08:31:38 -0300

    Últimas alterações

--- src/main/resources/db/migration/V2__Insert_default_users.sql
+++ src/main/resources/db/migration/V2__Insert_default_users.sql
@@ -1,4 +1,4 @@
 -- A senha para ambos é "admin"
-INSERT INTO users (username, email, password, role) VALUES
-('admin', 'admin@coruja.com', '$2a$10$8.UnVuG9HHgffUDAlk8qfeclM3eW5b1I0oFmOtPPgCvrSbouo0eue', 'admin'),
-('user', 'user@coruja.com', '$2a$10$8.UnVuG9HHgffUDAlk8qfeclM3eW5b1I0oFmOtPPgCvrSbouo0eue', 'user');
+INSERT INTO users (name , username, email, password, role) VALUES
+('Administrador','admin', 'admin@coruja.com', '$2a$10$8.UnVuG9HHgffUDAlk8qfeclM3eW5b1I0oFmOtPPgCvrSbouo0eue', 'admin'),
+('Usuario','user', 'user@coruja.com', '$2a$10$8.UnVuG9HHgffUDAlk8qfeclM3eW5b1I0oFmOtPPgCvrSbouo0eue', 'user');
